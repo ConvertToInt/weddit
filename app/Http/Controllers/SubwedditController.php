d098c12018b833f6f855353254f1bdefee97d68e
ConvertToInt <tdalgleish7@gmail.com>
2020-12-31 12:21:06 +0000

    create subweddit using post

--- app/Http/Controllers/SubwedditController.php
+++ app/Http/Controllers/SubwedditController.php
@@ -21,12 +21,12 @@ class SubwedditController extends Controller
         $request->validate([
             'title' => 'required|max:255',
             'bio' => 'required|max:64000',
-            'upload' => 'required|max:2000',
+            //'upload' => 'required|max:2000',
         ]);
 
         $upload = new Upload;
-        $upload->mimeType = $request->file('subweddit')->getMimeType();
-        $upload->path = $request->file('subweddit')->store('subweddits');
+        //$upload->mimeType = $request->file('subweddit')->getMimeType();
+        //$upload->path = $request->file('subweddit')->store('subweddits');
         $upload->title = $request->title;
         $upload->bio = $request->bio;
         $upload->user_id = Auth::id();
@@ -41,12 +41,4 @@ class SubwedditController extends Controller
 
         return view('subweddits.show', ['subweddit'=>$subweddit]);
     }
-    
-    
-    
-    /* public function show(Upload $upload,$origName=''){
-        $upload = Upload::findOrFail($upload->id);
-        //return response()->file(storage_path() . '/app/' . $upload->path);
-        return view('uploads.show', ['upload'=>$upload]); 
-    } */
 }
