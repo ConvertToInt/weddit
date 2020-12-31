8726375e8887c3983a1bf2bbec3894221ef25e78
ConvertToInt <tdalgleish7@gmail.com>
2020-12-31 09:00:31 +0000

    minor changes to logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="images/logo.jpg" alt="weddit logo" width="200px" height="200px"/>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
