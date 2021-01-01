3a30e759e487f645ac0a7e2a89e2525e75ddcfed
ConvertToInt <tdalgleish7@gmail.com>
2021-01-01 17:11:33 +0000

    relationships betweem user and post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function subweddit() {
+        return $this->belongsTo('App\Models\Subweddit');
+    }
 }
