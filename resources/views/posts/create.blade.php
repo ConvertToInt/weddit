dce81cdd1a70f15925310499fbbac8249524c9b8
ConvertToInt <tdalgleish7@gmail.com>
2021-01-08 16:56:53 +0000

    create post blade

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+
+@section('head')
+
+@endsection
+
+@section('content')
+
+<h1 class="title has-text-centered has-text-weight-bold is-size-3 has-text-grey-lighter mb-6">Create Post</h1>
+
+<div class="columns is-centered">
+    <div class="column is-half">
+    <form method ="POST" action={{url("/w/{$subweddit->name}")}} enctype ="multipart/form-data">
+        @csrf
+
+        <div class="field">
+            <label class="label is-medium has-text-grey-lighter">Title</label>
+            <div class="control">
+                <input class="input" type="text" name="title">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label is-medium has-text-grey-lighter">Body</label>
+            <div class="control">
+                <textarea class="textarea" name="body"></textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label is-medium has-text-grey-lighter">Thumbnail</label>
+            <div class="control">
+                <input class="input" type="file" name="thumbnail">
+            </div>
+        </div>
+
+        <div class="field is-grouped mt-5">
+        <div class="control">
+            <button class="button is-primary">Post to {{$subweddit->name}}</button>
+        </div>
+        <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+        </div>
+        </div>
+    </form>
+    </div>
+</div>
+@endsection
